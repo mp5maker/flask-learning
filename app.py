04b1f0da883444b4a99121be58f43899e654ea99
mp5maker <khan.photon@gmail.com>
2022-08-31 21:37:41 +0600

    :sparkles: feat: added a simple rendering of the html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
